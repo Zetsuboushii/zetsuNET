49e702238903e0b22ad58eb394d681c0334dfdc0
Zetsuboushii <zetsuboushii@gmail.com>
2025-03-15 15:34:14 +0100

    fix: removed unused code

--- freeze.py
+++ freeze.py
@@ -4,12 +4,5 @@ from app import app, load_from_json
 
 freezer = Freezer(app)
 
-@freezer.register_generator
-def holiday_urls():
-    holidays_list = load_from_json("calendar")
-
-    for holiday in holidays_list:
-        yield url_for('holidays', holiday_name=holiday['name'].lower().replace(" ", "-"))
-
 if __name__ == '__main__':
     freezer.freeze()

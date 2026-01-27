9781379f54c614f2f38f420877f6a26700a9d299
Zetsuboushii <zetsuboushii@gmail.com>
2026-01-27 17:28:26 +0100

    removed wip book

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def load_file(filename):
 @app.before_request
 def before_request():
     g.site_title = "ZetsuNET"
-    g.version = "b5.0"
+    g.version = "b5.1"
     g.git_hash = get_git_hash()
 
     g.static = "/static/"
